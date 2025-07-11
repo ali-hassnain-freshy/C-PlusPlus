38815b90941373f94cd92b050119e36b54a8e68c
AliHassnainFutureLeaders <maherhassnain70@gmail.com>
2025-07-11 10:45:49 -0700

    sa

---
+++ iday2/logical_operator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int number_and_even;
+    cout<<"Write A Even Number Less Than 10 (:";
+    cin >> number_and_even;
+    switch (number_and_even)
+    if (number_and_even<10 && number_and_even%2)
+    {
+        cout<< "thanks";
+    }
+    else if (number_and_even<10 | number_and_even%2)
+    {
+        cout<< "the number is not even or not less than six!";
+    }
+    
+    return 0;
+}
